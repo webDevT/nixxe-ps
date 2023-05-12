268969c391925248e9e13f0ffa91a927b7d3320a
Karina <karinadesyatnik27@gmail.com>
2023-05-12 13:58:43 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,11 @@ $('.menu-btn').click(function(){
     $(this).toggleClass('active');
     $('.menu').toggleClass('active');
 })
+$('.menu a').click(function(){
+    $('.menu-btn').removeClass('active');
+    $('.menu').removeClass('active');
+})
+
+
 
 });
